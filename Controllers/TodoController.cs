559677ee6605c133a7eb28c01e2e22e12cb342a9
thony <a0982830615@gmail.com>
2019-09-20 19:37:12 +0800

    Add>HttpPost,HttpPut,HttpDelete

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -17,19 +17,66 @@ namespace NetCore_RESTFulAPI.Controllers
         {
             _context = context;
 
-            if (_context.TodoItems.Count() == 0)
-            {
-                // Create a new TodoItem if collection is empty,
-                // which means you can't delete all TodoItems.
-                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
-                _context.SaveChanges();
-            }
+            // if (_context.TodoItems.Count() == 0)
+            // {
+            //     // Create a new TodoItem if collection is empty,
+            //     // which means you can't delete all TodoItems.
+            //     _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+            //     _context.SaveChanges();
+            // }
         }
         [HttpGet]
         public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
         {
             return await _context.TodoItems.ToListAsync();
         }
+
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> Inser(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();;
+            return CreatedAtAction(nameof(GetTodoItem), new { id = item.Id }, item);
+        }
+
+        
+        public ActionResult<TodoItem> Inser_舊寫法([FromBody]TodoItem post)
+        {
+            _context.TodoItems.Add(post);
+            _context.SaveChanges();
+            return post;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
 
         [Route("test")]
         [HttpGet]
