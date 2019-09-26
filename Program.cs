1d29d2816b08d6b59f0522e65a096d5cf60e7265
thony <a0982830615@gmail.com>
2019-09-26 15:20:49 +0800

    Add>db auto create

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using NetCore_RESTFulAPI.Models;
 
 namespace NetCore_RESTFulAPI
 {
@@ -14,7 +16,22 @@ namespace NetCore_RESTFulAPI
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            IWebHost host = CreateWebHostBuilder(args).Build();
+            using(var scope = host.Services.CreateScope() ){
+ var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<DBSContext>();
+                    context.Database.EnsureCreated();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
